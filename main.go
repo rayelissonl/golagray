78b65e93d62a7093dfb3b92c6d19abb338f30d1e
Rayelisson Lima <rayelissonl@gmail.com>
2024-07-27 22:34:49 -0300

    parte que falta terminar

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/rayelissonl/goapreapi/router"
+import (
+	"fmt"
+
+	"github.com/rayelissonl/goapreapi/config"
+	"github.com/rayelissonl/goapreapi/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		fmt.Println("config initialization error: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
